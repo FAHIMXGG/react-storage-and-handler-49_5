78d0a0c405dd25a85b9648dcc5ed7884fe20099d
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2023-03-25 03:34:10 +0600

    4

--- src/components/Cosmetic/Cosmetic.js
+++ src/components/Cosmetic/Cosmetic.js
@@ -2,12 +2,20 @@ import React from 'react';
 import './cosmetic.css'
 
 const Cosmetic = (props) => {
-    const {id, name, price} = props.cosmetic
+    const {id, name, price} = props.cosmetic;
+    const addToCart = (id) =>{
+        console.log('ggs', id)
+    }
+
+    const addToCartWithParam = () => addToCart(id)
     return (
         <div className='product'>
             <h2>Buy this: {name}</h2>
             <p>Only for: ${price}</p>
             <p><small>ID:{id}</small></p>
+            {/* <button onClick={addToCartWithParam}>Add co cart</button> */}
+            {/* shortcut */}
+            <button onClick={() => addToCart(id)}>Purchase</button>
         </div>
     );
 };
